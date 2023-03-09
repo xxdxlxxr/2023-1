5d26463c14f8bf01a9b4c4aec4bb28c5af59595c
xxdxlxxr <126651314+xxdxlxxr@users.noreply.github.com>
2023-03-09 16:58:16 +0900

    Add files via upload

---
+++ R programming and Lab./0309.R
@@ -0,0 +1,88 @@
+2 + 3
+2 * 3
+2 - 3
+2 / 3
+2^3
+3 - 2 * 1 + 4
+2 + 3; 2 * 3; 2 - 3; 2 / 3; 2^3
+(20 + 3 * 2) / 3
+options(digits = 10); (20 + 3 * 2) / 3
+
+pi
+PI
+sqrt(2); sqrt(100)
+sin(pi); cos(pi / 2); tan(pi / 4); sin(3.14); cos(1)
+asin(1); acos(1); atan(1)
+exp(1); exp(10); exp(100); exp(500); exp(1000); exp(10000)
+log(10); log(e); leg(exp(1)); log(exp(100)); log(exp(1000))
+log(10, base = 10); log(100, base = 10); log(2, base = 10); log(1000, base = 10)
+log(10, base = 3); log(100, base = 3); log(2, base = 3); log(1000, base = 3)
+abs(-3); abs(3); abs(-100)
+factorial(5); factorial(10); factorial(100); factorial(200);
+
+2^1023
+2^1024
+2^1023 + 2^1022
+
+3^100
+3^1000
+3^500
+3^750
+3^600
+3^670
+3^630
+3^650
+3^640
+3^645
+3^647
+3^646
+
+choose(5, 2); choose(10, 2); choose(100, 2); choose(1000, 2)
+
+log(e)
+log(exp(1))
+square(3)
+3^2
+squareroot(3)
+sqrt(3)
+sqrt(-3)
+a
+
+x1 = 9; x1
+x2 <- 18; x2
+x1 + x2; x1 - x2; x1 * x2; x2 / x1; x2^x1
+x = 7; x + 9
+e
+e <- exp(1); e
+y <- e^3; y
+z = x + y; z
+
+a = 100; a
+b <- 50; b
+c <- a + b; c
+d = a * b * c; d
+
+ls()
+a
+g
+
+1:20; seq(1:20); seq(1, 20)
+15:1; rev(1:15)
+seq(1, 10, by = 3); seq(1, 10, by = 0.3)
+seq(from = 2, to = 12, by = 3)
+rep(1, 15); rep(1:5, 5)
+rep(c(5, 3, 2), 3)
+a = rep(NA, 10); a
+
+k_score = 1:20
+sum(k_score); mean(k_score); median(k_score)
+max(k_score); min(k_score)
+range(k_score)
+var(k_score); sd(k_score)
+length(k_score)
+
+LETTERS[1:10]
+letters[1:10]
+month.name
+month.abb
+state.name
